351fbc672dc435aa03972f512356e07553139ae2
Edvard <elsberg1995@protonmail.com>
2021-05-26 15:17:38 +0200

    :zap

--- Program.cs
+++ Program.cs
@@ -1,12 +1,146 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
 using System.Collections.Generic;
-
 namespace project
 {
     class Program
     {
-        static void PrintMenu(){
+        static void Main()
+        {
+            using(var con = new MySqlConnection(GetLogin())){
+                con.Open();
+                using(var cmd = new MySqlCommand()){
+                    cmd.Connection = con;
+                    string sql = "";
+                    int dbNum;
+RunMenu:
+                    List<string> bufferDB = new List<string>();
+                    List<string> bufferTbl = new List<string>();
+                    cmd.CommandText = "SHOW DATABASES";
+                    using(MySqlDataReader rdr = cmd.ExecuteReader()){
+                        bufferDB.Add(rdr.GetName(0));
+                        while (rdr.Read()){
+                            bufferDB.Add(rdr.GetString(0));
+                        }
+                    }
+                    if(String.IsNullOrEmpty(con.Database) || !bufferDB.Contains(con.Database)){
+                        dbNum = GetInput(PrintBox(bufferDB, true, con.Database) + "Select database: ", bufferDB.Count-1);
+                        con.ChangeDatabase(bufferDB[dbNum]);
+                    }
+                    cmd.CommandText = "SHOW TABLES";
+                    using(MySqlDataReader rdr = cmd.ExecuteReader()){
+                        bufferTbl.Add(rdr.GetName(0));
+                        while (rdr.Read()){
+                            bufferTbl.Add(rdr.GetString(0));
+                        }
+                    }
+                    Console.Clear();
+                    Console.Write(PrintBox(bufferDB, false, con.Database));
+                    Console.Write(PrintBox(bufferTbl, false, ""));
+                    switch (GetDigit("1) Create Database\n2) Create Table\n3) Insert Row\n4) Delete Database\n5) Delete Table\n6) Delete Row\n7) Change using database\n8) Logout\n", 8))
+                    {
+                        case 1:
+                            sql = $"CREATE DATABASE {GetString("Type database name: ")}";
+                            cmd.CommandText = sql;
+                            cmd.ExecuteNonQuery();
+                            goto RunMenu;
+                        case 2:
+                            string table_name = GetString("Table name: ");
+                            int columnN = GetInput("Number of columns: ", 99);
+                            cmd.CommandText = $"CREATE TABLE {table_name}(";
+                            for(int i = 0; i < columnN; i++){
+                                string column_name = GetString($"Name column {i+1}: ");
+                                cmd.CommandText += $"{column_name} ";
+                                string column_type = GetString($"Type of column {column_name}: ");
+                                if(columnN-1 == i)
+                                    cmd.CommandText += $"{column_type} "; // type correct or die xd
+                                else
+                                    cmd.CommandText += $"{column_type}, "; // type correct or die xd
+                            }
+                            cmd.CommandText += $")";
+                            cmd.ExecuteNonQuery();
+                            goto RunMenu;
+                        case 3:
+                            int tblNum = GetInput(PrintBox(bufferTbl, true, "") + "Select table to insert row into: ", bufferTbl.Count-1);
+                            cmd.CommandText = $"INSERT INTO {bufferTbl[tblNum]}(";
+                            int columnValuesN = GetInput("Number of column values: ", 99);
+                            string columnNames = "";
+                            string columnValues = " VALUES(";
+                            for(int i = 0; i < columnValuesN; i++){
+                                string nameS = GetString("Name of column: ");
+                                columnNames += nameS;
+                                columnValues += GetString($"Remember ' ' around strings. Value of {nameS}: ");
+
+                                if(columnValuesN-1 != i){
+                                    columnNames += ",";
+                                    columnValues += ",";
+                                }
+                            }
+                            cmd.CommandText += $"{columnNames}) {columnValues})";
+                            cmd.ExecuteNonQuery();
+                            goto RunMenu;
+                        case 4:
+                            dbNum = GetInput(PrintBox(bufferDB, true, con.Database) + "Select database to delete: ", bufferDB.Count-1);
+                            sql = $"DROP DATABASE {bufferDB[dbNum]}";
+                            cmd.CommandText = sql;
+                            cmd.ExecuteNonQuery();
+                            goto RunMenu;
+                        case 5:
+                            int tbl = GetInput(PrintBox(bufferTbl, true, "") + "Select table to delete: ", bufferTbl.Count-1);
+                            cmd.CommandText = $"DROP TABLE {bufferTbl[tbl]}";
+                            cmd.ExecuteNonQuery();
+                            goto RunMenu;
+                        case 6:
+                            Console.WriteLine("Nothing here xD");
+                            Console.ReadKey(true);
+                            goto RunMenu;
+                        case 7:
+                            dbNum = GetInput(PrintBox(bufferDB, true, con.Database) + "Select database: ", bufferDB.Count-1);
+                            con.ChangeDatabase(bufferDB[dbNum]);
+                            goto RunMenu;
+                        case 8:
+                            break;
+                        default:
+                            goto RunMenu;
+                    }
+                }
+            }
+        }
+        static string PrintBox(List<string> buffer, bool showNum, string selected){
+            int selDBindex = buffer.IndexOf(selected);
+            string boxString = "";
+            int longest = 0;
+            int chCount = showNum ? 4 : 2;
+            foreach (var item in buffer)
+            {
+                if(item.Length > longest)
+                    longest = item.Length;
+            }
+            boxString += $"+{GetLine(longest+chCount)}+\n";
+            boxString += $"| {buffer[0]}{GetPadding(longest-buffer[0].Length+chCount-2)} |\n";
+            boxString += $"+{GetLine(longest+chCount)}+\n";
+            for(int i = 1; i < buffer.Count; i++){
+                string num = i > 9 ? $"{i})" : $"{i}) " ;
+                if(i == selDBindex)
+                    boxString += showNum ? $"|{num}{buffer[i]}*{GetPadding(longest-buffer[i].Length-1)} |\n" : $"| {buffer[i]}*{GetPadding(longest-buffer[i].Length-1)} |\n";
+                else
+                    boxString += showNum ? $"|{num}{buffer[i]}{GetPadding(longest-buffer[i].Length)} |\n" : $"| {buffer[i]}{GetPadding(longest-buffer[i].Length)} |\n";
+            }
+            return boxString += $"+{GetLine(longest+chCount)}+\n";
+        }
+        static string GetPadding(int length){
+            string line = "";
+            for(int i = 0; i < length; i++){
+                line += " ";
+            }
+            return line;
+        }
+        static string GetLine(int length){
+            string line = "";
+            for(int i = 0; i < length; i++){
+                line += "-";
+            }
+            return line;
         }
         public static int GetDigit(string message, int max)
         {
@@ -18,16 +152,12 @@ namespace project
             } while (number > max || number < 1);
             return number;
         }
-
         public static string GetLogin(){
-            //string server = GetString("Indtast server ip: ");
-            //string userid = GetString("Indtast userid: ");
-            //string password = GetPass();
-            //string database = GetString("Indtast database: ");
-
-            //return $"server={server};userid={userid};password={password};database={database}";
-            return @"server=localhost;userid=root;password=nBgPA6MHdp";
-
+            string server = GetString("Server ip: ");
+            string userid = GetString("Userid: ");
+            string password = GetPass();
+            //string database = GetString("Database: ");
+            return $"server={server};userid={userid};password={password}";
         }
         public static string GetString(string message)
         {
@@ -35,8 +165,6 @@ namespace project
             Console.Write(message);
             return Console.ReadLine();
         }
-
-
         public static int GetInput(string message, int max)
         {
             int number;
@@ -47,7 +175,6 @@ namespace project
             } while (!int.TryParse(Console.ReadLine(), out number) || number > max);
             return number;
         }
-
         // funktion til at lave stjerne GetPass
         public static string PrintStars(int starCount)
         {
@@ -57,7 +184,6 @@ namespace project
             }
             return stars;
         }
-
         public static string GetPass()
         {
             int starCount = 0;
@@ -66,7 +192,7 @@ namespace project
             do
             {
                 Console.Clear();
-                Console.Write("Indtast kode: {0}", PrintStars(starCount));
+                Console.Write("Password: {0}", PrintStars(starCount));
                 // tager keypress fra user putter ConsoleKeyInfo ind i keyinfo
                 ConsoleKeyInfo keyinfo = Console.ReadKey(true);
 
@@ -88,210 +214,5 @@ namespace project
             }while(true);
             return password;
         }
-
-        static void Main()
-        {
-            using(var con = new MySqlConnection(GetLogin())){
-                con.Open();
-                using(var cmd = new MySqlCommand()){
-                    cmd.Connection = con;
-                    string sql = "";
-RunMenu:
-                    List<string> bufferDB = new List<string>();
-                    cmd.CommandText = "SHOW DATABASES";
-                    using(MySqlDataReader rdr = cmd.ExecuteReader()){
-                        bufferDB.Add(rdr.GetName(0));
-                        while (rdr.Read()){
-                            bufferDB.Add(rdr.GetString(0));
-                        }
-                    }
-                    if(String.IsNullOrEmpty(con.Database)){
-                        con.ChangeDatabase(bufferDB[GetInput(PrintBox(bufferDB, true) + "Select database: ", bufferDB.Count-1)]);
-                    }
-                    Console.WriteLine(con.Database);
-                    switch (GetDigit(PrintBox(bufferDB, false) + "1) Create Database\n2) Create Table\n3) Insert Row\n4) Delete Database\n5) Delete Table\n6) Delete Row\n7) Change using database\n8) Logout\n", 8))
-                    {
-                        case 1:
-                            Console.Clear();
-                            sql = $"CREATE DATABASE {GetString("Indtast database navn: ")}";
-                            cmd.CommandText = sql;
-                            cmd.ExecuteNonQuery();
-                            goto RunMenu;
-                        case 2:
-
-                            goto RunMenu;
-                        case 3:
-
-                            goto RunMenu;
-                        case 4:
-                            Console.Clear();
-                            string inputString = GetString("Indtast database navn: ");
-                            if(bufferDB.Contains(inputString)){
-                                sql = $"DROP DATABASE {inputString}";
-                                cmd.CommandText = sql;
-                                cmd.ExecuteNonQuery();
-                                Console.Clear();
-                                Console.WriteLine("Success!");
-                            }else {
-                                Console.Clear();
-                                Console.WriteLine("Database does not exist!");
-                            }
-                            goto RunMenu;
-                        case 5:
-
-                            goto RunMenu;
-                        case 6:
-
-                            goto RunMenu;
-                        case 7:
-                            int dbNum = GetInput(PrintBox(bufferDB, true) + "Select database: ", bufferDB.Count-1);
-                            con.ChangeDatabase(bufferDB[dbNum]);
-                            goto RunMenu;
-                        case 8:
-                            break;
-
-                        default:
-                            goto RunMenu;
-                    }
-                }
-            }
-        }
-        static string PrintBox(List<string> buffer, bool showNum){
-            string boxString = "";
-            int longest = 0;
-            int chCount = showNum ? 4 : 2;
-            foreach (var item in buffer)
-            {
-                if(item.Length > longest)
-                    longest = item.Length;
-            }
-            boxString += $"+{GetLine(longest+chCount)}+\n";
-            boxString += $"| {buffer[0]}{GetPadding(longest-buffer[0].Length+chCount-2)} |\n";
-            boxString += $"+{GetLine(longest+chCount)}+\n";
-            for(int i = 1; i < buffer.Count; i++){
-                string num = i > 9 ? $"{i})" : $"{i}) " ;
-                boxString += showNum ? $"|{num}{buffer[i]}{GetPadding(longest-buffer[i].Length)} |\n" : $"| {buffer[i]}{GetPadding(longest-buffer[i].Length)} |\n";
-            }
-            return boxString += $"+{GetLine(longest+chCount)}+\n";
-        }
-        static string GetPadding(int length){
-            string line = "";
-            for(int i = 0; i < length; i++){
-                line += " ";
-            }
-            return line;
-        }
-        static string GetLine(int length){
-            string line = "";
-            for(int i = 0; i < length; i++){
-                line += "-";
-            }
-            return line;
-        }
     }
 }
-/*
-
-                    switch (GetDigit(PrintBox(bufferDatabases, true) + "1) Create Database\n2) Create Table\n3) Insert Row\n4) Delete Database\n5) Delete Table\n6) Delete Row\n7) Logout\n", 7))
-                    {
-                        case 1:
-                            Console.Clear();
-                            sql = $"CREATE DATABASE {GetString("Indtast database navn: ")}";
-                            cmd.CommandText = sql;
-                            cmd.ExecuteNonQuery();
-                            goto RunMenu;
-                        case 2:
-
-                            goto RunMenu;
-                        case 3:
-
-                            goto RunMenu;
-                        case 4:
-                            Console.Clear();
-                            string inputString = GetString("Indtast database navn: ");
-                            if(bufferDatabases.Contains(inputString)){
-                                sql = $"DROP DATABASE {inputString}";
-                                cmd.CommandText = sql;
-                                cmd.ExecuteNonQuery();
-                                Console.Clear();
-                                Console.WriteLine("Success!");
-                            }else {
-                                Console.Clear();
-                                Console.WriteLine("Database does not exist!");
-                            }
-                            goto RunMenu;
-                        case 5:
-
-                            goto RunMenu;
-                        case 6:
-
-                            goto RunMenu;
-                        case 7:
-                            break;
-
-                        default:
-                            goto RunMenu;
-                    }
-
-   string cs = @"server=localhost;userid=root;password=nBgPA6MHdp;database=test";
-   using (var con = new MySqlConnection(cs)){
-   con.Open();
-
-   using(var cmd = new MySqlCommand()){
-   cmd.Connection = con;
-
-   var stm = "SELECT VERSION()";
-   cmd.CommandText = stm;
-   var version = cmd.ExecuteScalar().ToString();
-   Console.WriteLine($"MySQL version : {con.ServerVersion}");
-   Console.WriteLine($"MySQL version : {version}");
-
-   cmd.CommandText = "DROP TABLE IF EXISTS test_table";
-   cmd.ExecuteNonQuery();
-
-   cmd.CommandText = @"CREATE TABLE test_table(id INT PRIMARY KEY AUTO_INCREMENT,
-   name TEXT, price INT)";
-   cmd.ExecuteNonQuery();
-
-   cmd.CommandText =  "INSERT INTO test_table(name, price) VALUES('Audi', 10001)";
-   cmd.ExecuteNonQuery();
-
-
-
-
-
-
-// this way stops sql injections and is faster
-var sql = "INSERT INTO test_table(name, price) VALUES(@name, @price)";
-cmd.CommandText = sql;
-
-cmd.Parameters.AddWithValue("@name", "BMW");
-cmd.Parameters.AddWithValue("@price", 30000);
-cmd.Prepare();
-cmd.ExecuteNonQuery();
-
-cmd.CommandText = "SELECT * FROM test_table";
-
-using(MySqlDataReader rdr = cmd.ExecuteReader()){
-
-Console.WriteLine($"{rdr.GetName(0),-4} {rdr.GetName(1),-10}{rdr.GetName(2),10}");
-
-while (rdr.Read()){
-Console.WriteLine($"{rdr.GetInt32(0),-4} {rdr.GetString(1),-10}{rdr.GetInt32(2),10}");
-}
-cmd.CommandText = "SELECT * FROM test_table";
-}
-cmd.CommandText = "SELECT * FROM test_table";
-using(MySqlDataReader rdr = cmd.ExecuteReader()){
-
-Console.WriteLine($"{rdr.GetName(0),-4} {rdr.GetName(1),-10}{rdr.GetName(2),10}");
-cmd.CommandText = "SELECT * FROM test_table";
-
-// read() method advaced the data reader to the next record(row). it returns true if thre are more rows otherwise false
-while (rdr.Read()){
-Console.WriteLine("{0} {1} {2}", rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2));
-}
-}
-}
-}
-*/
